db254bb6e023b5e446b201794b86b56824af16fd
Corey B <14075562+coreyb-git@users.noreply.github.com>
2026-03-10 13:37:58 +1000

    User can return nil, if they want, in the preprocessor, to mean 'show no SpellWarn diagnostics'

--- lua/spellwarn/diagnostics.lua
+++ lua/spellwarn/diagnostics.lua
@@ -47,7 +47,7 @@ function M.update_diagnostics(opts, bufnr)
     end
 
     -- Pre-process, if a function is set in opts to do anything.
-    diags = opts.func_preprocess(bufnr, diags)
+    diags = opts.func_preprocess(bufnr, diags) or {}
 
     -- TODO: Add suffix diagnostics with type of spelling error the way that LSP diagnostics do
     vim.diagnostic.set(namespace, bufnr, diags, opts.diagnostic_opts)
